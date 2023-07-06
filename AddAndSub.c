e4c530e741758f26ca97a7e0cc85ed56d8a617f7
Harsh <agarwalharsh2947.com>
2023-07-07 02:23:19 +0530

    product added

--- AddAndSub.c
+++ AddAndSub.c
@@ -3,6 +3,7 @@ int main() {
     int a,b;
     printf("Enter two numbers:\n");
     scanf("%d%d",&a,&b);
-    printf("Sum of two numbers is: %d" ,(a+b));
+    printf("Sum of two numbers is: %d\n" ,(a+b));
+    printf("The product of two numbers is %d", a*b);
     return 0;
 }
